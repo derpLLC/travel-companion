d8cfe7ca431ff2aeb51b3be078cc2cf8b84f5538
Sounav201 <sounav2001saha@gmail.com>
2021-10-06 14:14:52 +0530

    Initial app

---
+++ src/Api/index.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+
+  
+export const getPlacesData = async(type,sw,ne) => {
+
+    try {
+
+      const {data:{data}} = await  axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,{
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+          currency: 'INR',
+        },
+        headers: {
+          'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_TRAVEL_API_KEY,
+          'x-rapidapi-host': 'travel-advisor.p.rapidapi.com'
+        }
+      })
+
+        return data;
+        
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+export const getWeatherData = async (lat,lng) => {
+  try {
+
+    const {data} = await axios.get(`https://community-open-weather-map.p.rapidapi.com/find`, {
+      params: {
+           
+        lon: lng,
+        type: 'link, accurate',
+        lat: lat,
+        units: 'imperial, metric'
+      },
+      headers: {
+        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+        'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_WEATHER_API_KEY
+      }
+
+    })
+
+    return data;
+    
+  } catch (error) {
+    console.log(error)
+  }
+}
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useEffect, useState } from 'react'
+import { CssBaseline, Grid } from '@material-ui/core'
+import Header from './components/Header/Header'
+import List from './components/List/List'
+import Map from './components/Map/Map'
+import { getPlacesData, getWeatherData } from './Api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [places, setPlaces] = useState([]);
+    const [filteredPlaces, setfilteredPlaces] = useState([])
+    const [coordinates, setCoordinates] = useState({});
+    const [type, setType] = useState('restaurants')
+    const [rating, setRating] = useState('')
+    const [weatherData,setWeatherData] = useState([]);
+
+    const [childClicked, setChildClicked] = useState(null);
+    const [isLoading, setisLoading] = useState(false)
+    const [bounds, setBounds] = useState({});
+
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+            console.log({ latitude, longitude })
+            setCoordinates({ lat: latitude, lng: longitude })
+        })
+
+
+    }, [])
+
+
+    useEffect(() => {
+        const filterPlaces = places.filter((place) => place.rating > rating);
+
+        setfilteredPlaces(filterPlaces);
+
+
+    }, [rating])
+
+    useEffect(() => {
+
+        //console.log(coordinates,bounds)
+        if(bounds.sw && bounds.ne) {
+            setisLoading(true)
+
+            getWeatherData(coordinates.lat,coordinates.lng)
+                .then((data) => setWeatherData(data))
+
+            getPlacesData(type, bounds.sw, bounds.ne)
+                .then((data) => {
+                    //console.log('Places ',data)
+                    setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+                    setfilteredPlaces([]);
+                    setisLoading(false);
+                })
+        }
+    }, [type, bounds])
+
+    console.log('Places : ' ,places)
+    console.log('Filtered Places : ',filteredPlaces)
+
+    return (
+        <>
+            <CssBaseline />
+            <Header setCoordinates={setCoordinates} />
+            <Grid container spacing={3} style={{ width: '100%' }}>
+                <Grid item xs={12} md={4}>
+                    <List places={filteredPlaces.length ? filteredPlaces : places}
+                        childClicked={childClicked}
+                        isLoading={isLoading}
+                        type={type}
+                        setType={setType}
+                        rating={rating}
+                        setRating={setRating}
+                    />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                        places={filteredPlaces.length ? filteredPlaces : places}
+                        setChildClicked={setChildClicked}
+                        weatherData={weatherData}
+                    />
+                </Grid>
+
+
+
+            </Grid>
+        </>
+
+    )
 }
 
-export default App;
+export default App
